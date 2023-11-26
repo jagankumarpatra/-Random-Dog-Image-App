e9fd742975e85ecbf9b060de6b4cda1e31871ad0
jagankumarpatra <94950001+jagankumarpatra@users.noreply.github.com>
2023-11-26 09:49:14 +0530

    Your commit message here

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+// import { TaskComponent } from './task/task.component';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
 export class AppComponent {
-  title = 'angulartask';
+  public name="Vishwas"
+  title = 'Hello World';
+  public myId="testId";
+  public isdisabled=false;
+  public styler="jagan";
+  public hasError=false;
+  public isSpecial=true;
+  public highlightColor="green";
+  public greeting="";
+  onClick(){
+    alert("hi");
+    this.greeting="hi"; 
+  }
+  logmsg(value: string): void {
+  console.log(value);
+  alert(value);
+  }
+  public namee="";
+  displayName=false;
+  public color="rd";
+
+  public colors=["red","blue","green","yellow"];
+
+  public Name="jagaN";
 }
